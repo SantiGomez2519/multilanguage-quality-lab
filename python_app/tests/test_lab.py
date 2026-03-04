af06d935c1ccd00f5212dcf1506e188124ce3c80
SantiGomez2519 <sgomezo14@eafit.edu.co>
2026-03-04 09:38:38 -0500

    test: suma

--- python_app/tests/test_lab.py
+++ python_app/tests/test_lab.py
@@ -23,4 +23,11 @@ def test_es_palindromo_python():
 def test_es_palindromo_vacio():
     assert es_palindromo("")
 
+def test_suma_2_3():
+    assert suma(2, 3) == 5
 
+def test_suma_0_5():
+    assert suma(0, 5) == 5
+
+def test_suma_negativo_positivo():
+    assert suma(-2, 3) == 1
