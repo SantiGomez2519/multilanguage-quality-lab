520fb2625d7a92bb41b8ea31c4a9b202b84f8ed6
SantiGomez2519 <sgomezo14@eafit.edu.co>
2026-03-04 09:54:39 -0500

    refactor: unnecesary logic in suma

--- python_app/utils.py
+++ python_app/utils.py
@@ -1,9 +1,4 @@
-
 """Funciones utilitarias simples."""
 
 def suma(a, b):
-    # complejidad innecesaria (pero funcional)
-    if a == 0:
-        return b
-    else:
-        return a + b
+    return a + b
