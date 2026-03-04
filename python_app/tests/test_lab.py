0f675c851367be38c6064938b14699bd2159d0f2
SantiGomez2519 <sgomezo14@eafit.edu.co>
2026-03-04 09:38:25 -0500

    test: es palindromo

--- python_app/tests/test_lab.py
+++ python_app/tests/test_lab.py
@@ -4,8 +4,23 @@
 # - "radar" -> True, "anita lava la tina" -> True, "python" -> False, "" -> True, "Radar" -> True
 # - suma(2,3) -> 5; suma(0,5) -> 5; suma(-2,3) -> 1
 
-from palindromo import es_palindromo
-from utils import suma
+from ..palindromo import es_palindromo
+from ..utils import suma
 
-def test_ejemplo_siembra():
-    assert True
+
+def test_es_palindromo_radar():
+    assert es_palindromo("radar")
+
+def test_es_palindromo_Radar():
+    assert es_palindromo("Radar")
+
+def test_es_palindromo_anita_lava_la_tina():
+    assert es_palindromo("anita lava la tina")
+
+def test_es_palindromo_python():
+    assert not es_palindromo("python")
+
+def test_es_palindromo_vacio():
+    assert es_palindromo("")
+
+
