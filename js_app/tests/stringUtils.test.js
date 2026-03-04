a6cbb174f74302cf18fa086090604756060498c2
SantiGomez2519 <sgomezo14@eafit.edu.co>
2026-03-04 10:04:01 -0500

    test: palindromo js

--- js_app/tests/stringUtils.test.js
+++ js_app/tests/stringUtils.test.js
@@ -2,6 +2,24 @@
 // TODO: Escribe pruebas unitarias para isPalindrome.
 // Sugerencias: "radar" -> true; "anita lava la tina" -> true; "python" -> false; "" -> true; "Radar" -> true
 
-test('ejemplo siembra', () => {
-  expect(true).toBe(true);
+const { isPalindrome } = require('../src/stringUtils');
+
+test('isPalindrome radar', () => {
+  expect(isPalindrome('radar')).toBe(true);
+});
+
+test('isPalindrome Radar', () => {
+  expect(isPalindrome('Radar')).toBe(true);
+});
+
+test('isPalindrome anita lava la tina', () => {
+  expect(isPalindrome('anita lava la tina')).toBe(true);
+});
+
+test('isPalindrome python', () => {
+  expect(isPalindrome('python')).toBe(false);
+});
+
+test('isPalindrome empty', () => {
+  expect(isPalindrome('')).toBe(true);
 });
