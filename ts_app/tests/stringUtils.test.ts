7905d651edcdbf9e0870d01e2352c70c717b76cf
SantiGomez2519 <sgomezo14@eafit.edu.co>
2026-03-04 10:10:49 -0500

    test: palindromo ts

--- ts_app/tests/stringUtils.test.ts
+++ ts_app/tests/stringUtils.test.ts
@@ -9,3 +9,25 @@ describe('siembra', () => {
     expect(true).toBe(true)
   })
 })
+
+describe('isPalindrome', () => {
+  it('isPalindrome radar', () => {
+    expect(isPalindrome('radar')).toBe(true);
+  });
+
+  it('isPalindrome Radar', () => {
+    expect(isPalindrome('Radar')).toBe(true);
+  });
+
+  it('isPalindrome anita lava la tina', () => {
+    expect(isPalindrome('anita lava la tina')).toBe(true);
+  });
+
+  it('isPalindrome python', () => {
+    expect(isPalindrome('python')).toBe(false);
+  });
+
+  it('isPalindrome empty', () => {
+    expect(isPalindrome('')).toBe(true);
+  });
+});
